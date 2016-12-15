bee10cc53e29b77f338c63a2a2a357a8142afc60
黄思程 <si-cheng.huang@utsoft.cn>
2016-12-15 16:28:03 +0800

    新建项目，初始化仓库

---
+++ app/src/main/java/com/utouu/easyjoke/BaseApplication.java
@@ -0,0 +1,29 @@
+package com.utouu.easyjoke;
+
+import android.app.Application;
+
+import me.drakeet.library.CrashWoodpecker;
+import me.drakeet.library.PatchMode;
+
+/**
+ * Create by 黄思程 on 2016/12/15   16:20
+ * Function：
+ * Desc：
+ */
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //内存泄漏检查初始化
+        CrashWoodpecker.instance()
+                .withKeys("widget", "me.drakeet")
+                .setPatchMode(PatchMode.SHOW_LOG_PAGE)
+                .setPatchDialogUrlToOpen("https://drakeet.me")
+                .setPassToOriginalDefaultHandler(true)
+                .flyTo(this);
+
+
+    }
+}
